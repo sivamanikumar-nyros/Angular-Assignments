261fd6d842320e429f2d94d89108db143a12a06c
sivamanikumar-nyros <siva.nyros@gmail.com>
2019-09-23 11:15:09 +0530

    initial-commit

--- cmp-databinding-final/src/app/app.module.ts
+++ cmp-databinding-final/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,

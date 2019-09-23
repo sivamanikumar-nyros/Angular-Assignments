2faa9032147b6f66be9b812338c19f706a5eeac4
sivamanikumar-nyros <siva.nyros@gmail.com>
2019-09-23 11:33:48 +0530

    initial commit

---
+++ office/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    HeroFormComponent,
+    BasicHighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ office/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+	selector: '[appBasicHighlight]'	
+})
+
+export class BasicHighlightDirective implements OnInit {
+
+	constructor(private elementRef: ElementRef){
+
+	}
+	
+	ngOnInit() {
+		// this.elementRef.nativeElement.style.backgroundColor = 'skyblue';
+		this.elementRef.nativeElement.style.color = 'green';
+	}
+
+}

70c7018fad330cb6344fc99d31d4557d19376f65
sivamanikumar-nyros <siva.nyros@gmail.com>
2019-09-23 10:47:42 +0530

    initial-commit

---
+++ myshop/src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+
+	// Type#1
+	// public name: string;
+	// public amount: number;
+
+	// constructor(name: string, amount: number){
+	// 	this.name = name;
+	// 	this.amount = amount;
+	// }
+
+	// Type#2
+	constructor(public name: string, public amount: number){}
+}

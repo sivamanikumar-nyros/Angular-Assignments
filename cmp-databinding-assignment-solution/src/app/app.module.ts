769a5585b5d5be7ab3ecde7d56ffa5dddec90521
sivamanikumar-nyros <siva.nyros@gmail.com>
2019-09-23 11:22:30 +0530

    initial commit

--- cmp-databinding-assignment-solution/src/app/app.module.ts
+++ cmp-databinding-assignment-solution/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CockpitComponent,
-    ServerElementComponent
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
@@ -18,4 +20,4 @@ import { ServerElementComponent } from './server-element/server-element.componen
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
